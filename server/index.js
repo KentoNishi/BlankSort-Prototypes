9955ae016921fa7c0bd263b770181d8cbd697d0b
Kento Nishi <kento24gs@outlook.com>
2019-11-09 14:34:56 -0800

    Redeploying server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     app = express();
 
-// app.use("../tests", express.static("tests"));
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
