492597fc5d50793d09beb23177a942560c226d02
Kento Nishi <kento24gs@outlook.com>
2019-11-15 21:24:42 -0800

    Added directory list

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import glob
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    os.chdir(os.getcwd())
+    x = [file for file in glob.glob("**/*.*")]
+    return "Hello World!\n"+str(x)
