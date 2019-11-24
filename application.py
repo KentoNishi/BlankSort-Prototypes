05513b9b1a451bd74e1ce53dbe1ad4096621a88e
Kento Nishi <kento24gs@outlook.com>
2019-11-23 16:18:58 -0800

    Added build script

--- application.py
+++ application.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[37]:
+
+
 import os
 from keras.models import load_model
 import pickle
@@ -5,17 +11,55 @@ import re
 import numpy as np
 import nltk
 from nltk.corpus import stopwords
-model = load_model(os.path.join(os.path.join(os.path.abspath('../binaries'),"models"),"classification_model.h5"))
-names=["window_size","vocab_size","dictionary"]
-for name in names:
-    with open(os.path.join(os.path.join(os.path.abspath('../binaries'),"pickles"),name+".pickle"), "rb") as f:
-        globals()[name]=pickle.load(f)
-nltk.download("stopwords")
-stopWords = set(stopwords.words("english"))
+
+
+# In[ ]:
+
+
+def inNB():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
+
+# In[39]:
+
+
+def beforeStartup():
+    if(inNB()):
+        maybe_download("binaries.zip",
+                    "https://blanksortbinaries.blob.core.windows.net/binaries/binaries.zip")
+        extract("binaries.zip")
+    globals()["model"] = load_model(os.path.join(os.path.join(os.path.abspath(
+        './binaries'), "models"), "classification_model.h5"))
+    model.summary()
+    names = ["window_size", "vocab_size", "dictionary"]
+    for name in names:
+        with open(os.path.join(os.path.join(os.path.abspath('./binaries'), "pickles"), name+".pickle"), "rb") as f:
+            globals()[name] = pickle.load(f)
+    nltk.download("stopwords")
+    globals()["stopWords"] = set(stopwords.words("english"))
+
+
+# In[41]:
+
+
 def removeStopWords(rankedWords):
     return [word for word in rankedWords if word[0] not in stopWords]
+
+
+# In[42]:
+
+
 def removeNumbers(rankedWords):
     return  [word for word in rankedWords if not any(char.isdigit() for char in word[0])]
+
+
+# In[43]:
+
+
 def generateInverseCounts(wordSequence):
     countDict=dict()
     for word in wordSequence:
@@ -24,6 +68,11 @@ def generateInverseCounts(wordSequence):
         else:
             countDict[word]+=1
     return [1/countDict[word] for word in wordSequence]
+
+
+# In[44]:
+
+
 def findWordScores(wordSequence,tokenized):
     wordScores=np.zeros(len(wordSequence))
     contextCounts=np.zeros(len(wordSequence))
@@ -37,6 +86,11 @@ def findWordScores(wordSequence,tokenized):
             wordScores[i]+=confidence[0][0]
             wordScores[k]+=confidence[0][0]
     return ([tokenized[i],inverseCounts[i]*wordScores[i]/contextCounts[i]] for i in range(len(wordSequence)))
+
+
+# In[45]:
+
+
 def formatData(string):
     string=re.sub(r"[^\w\s]", ' ', string)
     string=string.lower()
@@ -48,6 +102,11 @@ def formatData(string):
         else:
             wordSequence.append(dictionary["UNK"])
     return wordSequence,tokenized,string
+
+
+# In[46]:
+
+
 def generateRankedList(string,**attributes):
     string=" ".join(string.split())
     noNumbers=attributes["noNumbers"] if "noNumbers" in attributes else True
@@ -67,6 +126,11 @@ def generateRankedList(string,**attributes):
     if noNumbers:
         rankedWords=removeNumbers(rankedWords)
     return rankedWords
+
+
+# In[47]:
+
+
 def printList(testCase,num=None,**attributes):
     noNumbers=attributes["noNumbers"] if "noNumbers" in attributes else True
     noStopWords=attributes["noStopWords"] if "noStopWords" in attributes else True
@@ -80,3 +144,62 @@ def printList(testCase,num=None,**attributes):
     for i in range(num):
         print(str(i+1)+". "+rankedWords[i][0]+": "+str(round(rankedWords[i][1],3)))
     print()
+
+
+from flask import Flask, request, jsonify
+import glob
+import urllib
+import zipfile
+import re
+import io
+import cgi
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def maybe_download(filename, url):
+    if not os.path.exists(os.path.join(os.getcwd(), filename)):
+        filename, _ = urllib.request.urlretrieve(
+            url, os.path.join(os.getcwd(), filename))
+    statinfo = os.stat(os.path.join(os.getcwd(), filename))
+    print('Found and verified', filename)
+    return filename
+
+
+def extract(filename):
+    with zipfile.ZipFile(filename, "r") as zip_ref:
+        zip_ref.extractall("")
+
+def returnRanks(testCase, num=None, **attributes):
+    noNumbers = attributes["noNumbers"] if "noNumbers" in attributes else True
+    noStopWords = attributes["noStopWords"] if "noStopWords" in attributes else True
+    rankedWords = generateRankedList(
+        testCase, noNumbers=noNumbers, noStopWords=noStopWords)
+    return rankedWords
+
+
+@app.before_first_request
+def beforeFirstRequest():
+    beforeStartup()
+
+
+@app.route("/post", methods=['POST'])
+def loadPage():
+    data = request.json
+    result  = {"status": "error"}
+    if("text" in data and len(data["text"]) <= 3000):
+        try:
+            result = {
+                "status": "ok",
+                "result": returnRanks(data["text"])
+            }
+        except Exception:
+            pass
+    return jsonify(result)
+
+
+@app.route("/")
+def loadDefault():
+    return "Server running!"
---
+++ scripts/buildScripts.py
@@ -0,0 +1,73 @@
+import subprocess
+import os
+
+p = subprocess.Popen("jupyter nbconvert --to python "+os.path.join(os.path.join(os.path.dirname(os.path.dirname(__file__)),"notebooks","runModel.ipynb"))+" --stdout", stdout=subprocess.PIPE)
+
+runModelScript=p.communicate()[0].decode("utf-8")
+
+serverInit="""
+from flask import Flask, request, jsonify
+import glob
+import urllib
+import zipfile
+import re
+import io
+import cgi
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+def maybe_download(filename, url):
+    if not os.path.exists(os.path.join(os.getcwd(), filename)):
+        filename, _ = urllib.request.urlretrieve(
+            url, os.path.join(os.getcwd(), filename))
+    statinfo = os.stat(os.path.join(os.getcwd(), filename))
+    print('Found and verified', filename)
+    return filename
+
+
+def extract(filename):
+    with zipfile.ZipFile(filename, "r") as zip_ref:
+        zip_ref.extractall("")
+"""
+
+serverRun="""
+def returnRanks(testCase, num=None, **attributes):
+    noNumbers = attributes["noNumbers"] if "noNumbers" in attributes else True
+    noStopWords = attributes["noStopWords"] if "noStopWords" in attributes else True
+    rankedWords = generateRankedList(
+        testCase, noNumbers=noNumbers, noStopWords=noStopWords)
+    return rankedWords
+
+
+@app.before_first_request
+def beforeFirstRequest():
+    beforeStartup()
+
+
+@app.route("/post", methods=['POST'])
+def loadPage():
+    data = request.json
+    result  = {"status": "error"}
+    if("text" in data and len(data["text"]) <= 3000):
+        try:
+            result = {
+                "status": "ok",
+                "result": returnRanks(data["text"])
+            }
+        except Exception:
+            pass
+    return jsonify(result)
+
+
+@app.route("/")
+def loadDefault():
+    return "Server running!"
+"""
+
+with open(os.path.join(os.path.join(os.path.dirname(os.path.dirname(__file__)),"application.py")),"w") as file:
+    file.write(runModelScript+serverInit+serverRun)
+    file.close()
+
