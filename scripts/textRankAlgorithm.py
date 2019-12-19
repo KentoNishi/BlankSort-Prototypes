017c5a54bdc824ed9c9b40a57b72cd842b0d80e1
Kento Nishi <kento24gs@outlook.com>
2019-12-19 12:56:11 -0800

    Added TextRank Algorithm

---
+++ scripts/textRankAlgorithm.py
@@ -0,0 +1,18 @@
+import spacy
+import pytextrank
+
+def rank(text):
+    # load a spaCy model, depending on language, scale, etc.
+    nlp = spacy.load("en_core_web_sm")
+
+    # add PyTextRank to the spaCy pipeline
+    tr = pytextrank.TextRank()
+    nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
+
+    doc = nlp(text)
+
+    # examine the top-ranked phrases in the document
+    for p in doc._.phrases:
+        print("{:.4f} {:5d}  {}".format(p.rank, p.count, p.text))
+        print(p.chunks)
+    return [(p.rank,p.text) for p in doc._.phrases]
