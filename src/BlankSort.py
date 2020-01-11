bc1e068a43887d29fe20d6f0966e82bf152a8c8b
Kento Nishi <kento24gs@outlook.com>
2020-01-10 22:10:08 -0800

    Finished implementing tokenization

--- src/BlankSort.py
+++ src/BlankSort.py
@@ -24,6 +24,7 @@ class BlankSort:
     def __loadData(self, binary_path):
         nltk.download("wordnet")
         nltk.download("stopwords")
+        nltk.download("punkt")
         self.__model = FTmmap(
             os.path.join(os.getcwd(), os.path.join(binary_path, "cc.en.300.vec"))
         )
@@ -44,14 +45,31 @@ class BlankSort:
     def __cleanText(self, text):
         text = text.lower()
         tokens = nltk.word_tokenize(text)
+        tokens = [
+            word for word in tokens if word not in self.__stops and word.isalpha()
+        ]
+        tokens = [word.strip() for word in tokens]
         lemmatizedWords = [self.__lemmatizer.lemmatize(word) for word in tokens]
         stemmedWords = [
             token
             for token in lemmatizedWords
             if self.__stemmer.stem(token) not in self.__stops
-            and token not in self.__stops
+            and self.__model.inVocab(token)
         ]
         return stemmedWords
 
+    def __countWords(self, tokens):
+        wordCounts = dict()
+        for i in range(len(tokens)):
+            if tokens[i] in wordCounts:
+                wordCounts[tokens[i]] += 1
+            else:
+                wordCounts[tokens[i]] = 1
+        return wordCounts
+
     def rank(self, text):
-        text = self.__cleanText(text)
+        tokens = self.__cleanText(text)
+        print(tokens)
+        wordCounts = self.__countWords(tokens)
+        scores = np.zeros(len(tokens))
+
